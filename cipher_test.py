8b46bdc0603a71ad24819a179320acc6271afbd2
Ciaran Downey <me@ciarand.me>
2015-03-04 18:39:42 -0800

    Add documentation to cipher_test.py

--- cipher_test.py
+++ cipher_test.py
@@ -4,9 +4,17 @@ from caesar_cipher import CaesarCipher
 from vigenre_cipher import VigenreCipher
 
 def generate_random(n):
+    """
+    Generates a random string, n characters long.
+    """
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase) for _ in range(n))
 
 def run_cipher_tests(cipher, keyfunc):
+    """
+    Runs a series of random strings through the provided cipher. Each iteration
+    the cipher's set_key function is called with the output of the keyfunc
+    callback (which is passed the current iteration index).
+    """
     for i in range(1, 100):
         # create the key according to the passed in keyfunc
         cipher.set_key(keyfunc(i))
